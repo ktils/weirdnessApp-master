1db178acd93e0ee3515c43d4ecb6170a4343e425
Lakunta Tillis <kt_tillis@hotmail.com>
2019-05-11 23:11:23 -0500

    Deleted updates for redux implentation

--- src/components/GifPopulation.js
+++ src/components/GifPopulation.js
@@ -1,9 +1,5 @@
 import React, {Component} from 'react';
 import GifPics from '../components/GifPics';
-import {bindActionCreators} from 'redux';
-import {connect} from 'react-redux';
-import {SelectedGif} from '../actions/SelectedGif';
-import {App} from '../App';
 
 const GifPopulation = (props) => {
     const gifpics = props.gifs.map((image, index) => {
@@ -18,14 +14,4 @@ const GifPopulation = (props) => {
     );
   };
 
-  function mapStateToProps(props){
-    return {
-      gif: props.gifs
-    };
-  }
-
-  function matchDispatchToProps(dispatch){
-    return bindActionCreators({SelectedGif: SelectedGif}, dispatch)
-  }
-
-export default connect(mapStateToProps, matchDispatchToProps)(GifPopulation);
+export default GifPopulation;
